f4f91edad467cb37d837d0134d0b1dc19b83c4e6
Pablex05 <p.fracaro@alumno.um.edu.ar>
2022-06-01 19:40:46 -0300

    update

--- resources/examen_fisico_densidad.py
+++ resources/examen_fisico_densidad.py
@@ -5,8 +5,11 @@ from database.model import ExamenFisicoDensidad
 class ExamenFisicoDensidadApi(Resource):
 
     def get(self, id):
-        examen_fisico_densidad = ExamenFisicoDensidad.objects.get(id=id).to_json()
-        return Response(examen_fisico_densidad, mimetype="application/json", status=200)
+        try:
+            examen_fisico_densidad = ExamenFisicoDensidad.objects.get(id=id).to_json()
+            return Response(examen_fisico_densidad, mimetype="application/json", status=200)
+        except:
+            return 'get not found!', 404
 
     def put(self, id):  
         try:
@@ -19,20 +22,30 @@ class ExamenFisicoDensidadApi(Resource):
             return 'update error!', 500
 
     def delete(self, id):
-        ExamenFisicoDensidad.objects.get(id=id).delete()
-        return 'delete OK!', 200
+        try:
+            examen_fisico_densidad = ExamenFisicoDensidad.objects.get(id=id)
+            examen_fisico_densidad.delete()
+            # ExamenFisicoDensidad.objects.get(id=id).delete()
+            return 'delete OK!', 200
+        except:
+            return 'delete error!', 500
 
 class ExamenesFisicosDensidadApi(Resource):
 
     def get(self):
+        try:
         # traer todo el examen fisico densidad
-        examen_fisico_densidad = ExamenFisicoDensidad.objects().to_json()
-        return Response(examen_fisico_densidad, mimetype="application/json", status=200)
+            examen_fisico_densidad = ExamenFisicoDensidad.objects().to_json()
+            return Response(examen_fisico_densidad, mimetype="application/json", status=200)
+        except:
+            return 'get not found!', 404
 
     def post(self):
-        body = request.get_json()
-        examen_fisico_densidad = ExamenFisicoDensidad(**body).save()
-        id = examen_fisico_densidad.id
-        return {'id': str(id)}, 200
-    
+        try:
+            body = request.get_json()
+            examen_fisico_densidad = ExamenFisicoDensidad(**body).save()
+            id = examen_fisico_densidad.id
+            return {'id': str(id)}, 200
+        except:
+            return 'post error!', 500
     
